68b02189290d735430127d5d762dde6eaffb7b93
DenizBasgoren <denizbasgoren1997@gmail.com>
2023-05-25 21:56:51 +0300

    inst_mem integration

--- src/cpu.v
+++ src/cpu.v
@@ -6,11 +6,11 @@ input wire rst;
 
 wire [31:0] r_for_pc;
 wire [22:0] cword;
-reg [31:0] inst;
+wire [31:0] inst;
 wire [31:0] imm;
 wire [31:0] pc;
 
 control_unit cu (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm), .inst(inst), .r_for_pc(r_for_pc));
 Datapath dp (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm), .r_for_pc(r_for_pc));
-
+instr_mem im (.r_addr_imem(pc), .r_data_imem(inst));
 endmodule
---
+++ src/instr_mem.v
@@ -0,0 +1,19 @@
+
+
+module instr_mem(
+    input [6:0] r_addr_imem,
+    output reg [31:0] r_data_imem
+);
+
+reg [7:0] imem [127:0];
+//param_mux #(.input_length(32), .X_to_1(32)) mux(.i_mux(imem), .select(r_addr_imem), .o_mux(r_data_imem));
+`ifndef COCOTB_SIM
+    initial $readmemh("../../test.mem", imem);
+`endif
+`ifdef COCOTB_SIM
+    initial $readmemh("../../zero_inst.mem", imem);
+`endif
+always @(*) begin
+    r_data_imem = {imem[r_addr_imem + 3],imem[r_addr_imem + 2],imem[r_addr_imem + 1],imem[r_addr_imem]};    
+end
+endmodule
