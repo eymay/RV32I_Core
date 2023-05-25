a9aefd8522323f3ed11dc5b7c0e475d7af550b36
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-05-25 19:58:39 +0300

    cpu fixed

--- src/cpu.v
+++ src/cpu.v
@@ -1,12 +1,14 @@
 
-module cpu (clk, rst)
+module cpu (
+    input clk,
+    input rst);
 
 wire [31:0] r_for_pc;
 wire [22:0] cword;
 wire [31:0] inst;
 wire [31:0] imm;
 
-control_unit u1 (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm))
-Datapath u2 (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm), .r_for_pc(r_for_pc));
+control_unit cu (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm));
+Datapath dp (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm), .r_for_pc(r_for_pc));
 
 endmodule
