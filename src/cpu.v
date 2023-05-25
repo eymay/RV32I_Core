fc138d0dd3862333ab8b82adab4216c460ca94c4
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-05-25 20:20:20 +0300

    fix

--- src/cpu.v
+++ src/cpu.v
@@ -7,6 +7,7 @@ wire [31:0] r_for_pc;
 wire [22:0] cword;
 wire [31:0] inst;
 wire [31:0] imm;
+wire [31:0] pc;
 
 control_unit cu (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm));
 Datapath dp (.clk(clk), .rst(rst), .cword(cword), .pc(pc), .imm(imm), .r_for_pc(r_for_pc));
