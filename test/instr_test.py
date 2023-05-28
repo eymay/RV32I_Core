655311a8ac659fa8d02803e94b40f2c61f369930
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-05-28 17:21:21 +0300

    Rename to emphasize generality

--- test/instr_test.py
+++ test/instr_test.py
@@ -12,11 +12,11 @@ for file in glob.glob(os.path.join(tests_dir, "../src/*.v")):
 #    txtfiles.append(file)
 @pytest.mark.skipif(os.getenv("SIM") == "ghdl", reason="Verilog not suported")
 # @pytest.mark.parametrize("seed", range(10))
-def test_add_verilog():
+def unit_test_instr_verilog():
     run(verilog_sources=txtfiles, 
         work_dir=os.path.join(tests_dir, "cocotb"),
         toplevel="ALU_system",
-        module="add_cocotb", waves=True)  # sources  # top level HDL  # name of cocotb test module
+        module="unit_test_instrs", waves=True)  # sources  # top level HDL  # name of cocotb test module
 
 if __name__ == "__main__":
-    test_add_verilog()
+    unit_test_instr_verilog()
